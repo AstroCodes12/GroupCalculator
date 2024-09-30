dafdea5a6c3bc307432df50dd13102a633fb0f48
IIIIlIlllll <kevin20070312@icloud.com>
2024-09-30 10:17:09 -0700

    Update Calculator.swift

--- Calculator.swift
+++ Calculator.swift
@@ -1,6 +1,6 @@
 import Foundation
-
 // Menu function to display options to the user
+import Glibc
 func showMenu() {
 print("""
 Select an operation:
@@ -12,4 +12,32 @@ Select an operation:
 6: Remainder
 """)
 }
+
 showMenu() //displays menu
+print("Enter Function Number Here:")
+var funcNumber = Int(readLine()!)!
+
+print("Now enter your two numbers:")
+let numberA = Double(readLine()!)!
+let numberB = Double(readLine()!)!
+
+if funcNumber == 6{
+    print(Int(numberA) % Int(numberB))
+}
+else if funcNumber == 5 {
+  let result = Int(pow(numberA, numberB))
+  print(result)
+}else if funcNumber == 3{
+   print(numberA * numberB)
+
+}else if funcNumber == 4{
+ if numberB == 0{
+  print("Don't have real number answer")
+ }else{
+       print(numberA / numberB)
+}
+}else if funcNumber == 2{
+    print(numberA - numberB)
+}else if funcNumber == 1{
+    print(numberA + numberB)
+}
